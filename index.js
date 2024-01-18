ee66b2a4676c506516842628e3f8da7f2509c079
Dantas Crispim <dantas10.crispim@gmail.com>
2024-01-18 09:44:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // essa função e pra adicionar o cliente
+// Adicionando function ao index.JS
 
-
-function addNewContact() {
-
+function hello(){
+  console.log("Oi, Seja Bem vindo")
 }
+
+hello()
+
+
